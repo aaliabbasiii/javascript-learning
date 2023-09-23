60da10253be5a1ab4eaea05202fbf65d222eb08c
aaliabbasiii <111428354+aaliabbasiii@users.noreply.github.com>
2023-09-23 17:10:41 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+"use strinct"  //treat all javascript code as newer version
+let name = "Aali"
+let username = "aaliabbasi"
+let age = 21
+let isPlaced = true;
+let cgpa = 9.1
+
+console.log(name, username, age, isPlaced, cgpa);
