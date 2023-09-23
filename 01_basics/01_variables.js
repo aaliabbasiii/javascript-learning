eda9e66f161846f6ece265a912bbbf6705f5d368
aaliabbasiii <111428354+aaliabbasiii@users.noreply.github.com>
2023-09-23 17:15:27 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 380068
+let accountName = "Aali Abbasi"
+var accountEmail = "aali@gmail.com"
+accountCity = "Gurugram"
+let accountState;
+
+console.log(accountId)
+console.log(accountName);
+console.log(accountEmail);
+console.log(accountCity);
+
+console.table([accountId , accountName, accountEmail, accountCity, accountState])
